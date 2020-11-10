a8d7d266704c234bf010491729075c33888d1b27
sru-be <71267303+sru-be@users.noreply.github.com>
2020-11-10 10:32:57 +0100

    update product service

--- backend/src/services/product.service.ts
+++ backend/src/services/product.service.ts
@@ -1,8 +1,12 @@
 import { ProductAttributes, Product } from '../models/product.model';
 import { User } from '../models/user.model';
 
+const { Op } = require('sequelize');
+
 export class ProductService {
 
+    // public getById(productID: number): Promise<Product> {
+    // return Product.findByPk(productId).
     public getById(id: string): Promise<Product> {
         return Product.findByPk(parseInt(id, 10))
             .then(found => {
@@ -24,7 +28,10 @@ export class ProductService {
     }
 
     public delete(productId: number): Promise<Product> {
-
+        // return Product.findByPk(productId)
+        // .then(found => found.destroy()
+        // .then(() => Promise.resolve(found))
+        // .catch(err => Promise.reject(err)));
         const product: Promise<Product> = Product.findOne({
             where: { productId: productId }
         });
@@ -34,13 +41,57 @@ export class ProductService {
         return product;
     }
 
-    public modify(product: ProductAttributes): Promise<ProductAttributes> {
-        // TODO
-        return;
+    public modify(productId: number, product: ProductAttributes): Promise<ProductAttributes> {
+        return Product.findByPk(productId).then(found => found.update(product)
+            .then(() => {
+                return Promise.resolve(found);
+            }).catch(err => Promise.reject(err)));
     }
 
     public getAll(): Promise<Product[]> {
-        return Product.findAll();
+        return Product.findAll()
+            .catch(err => Promise.reject(err));
     }
+
+    public getPending(): Promise<Product[]> {
+        return Product.findAll({
+            where: {
+                isApproved: 'pending'
+            },
+        }).catch(err => Promise.reject(err));
+    }
+
+    public getApproved(): Promise<Product[]> {
+        return Product.findAll({
+            where: {
+                isApproved: 'approved'
+            },
+        }).catch(err => Promise.reject(err));
+    }
+
+    public getDisapproved(): Promise<Product[]> {
+        return Product.findAll({
+            where: {
+                isApproved: 'disapproved'
+            },
+        }).catch(err => Promise.reject(err));
+    }
+
+     public getByUser(userName: string): Promise<Product[]> {
+        return Product.findAll({
+            where: {
+                userName: userName
+            },
+     }).catch (err => Promise.reject(err));
+     }
+
+    // public getByCateogry(cateogry: string): Promise<Product[]> {
+    //    return Product.findAll({
+    //        where: {
+    //            category: cateogry
+    //        },
+    // }).catch (err => Promise.reject(err));
+    // }
+
 
 }
